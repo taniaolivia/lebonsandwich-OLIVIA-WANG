af72c58d0bcb8eccff1310993be43ed671b4ef58
Ziyi WANG <ziyiwang1027@gmail.com>
2022-02-04 21:39:45 +0100

    ajoute validation des données

--- service/api/app.js
+++ service/api/app.js
@@ -6,6 +6,7 @@ const hateoasLinker = require('express-hateoas-links');
 const db = require('./knex.js');
 const uuid = require('uuid');
 const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const token = require('crypto-token');
 const indexRouter = require('./routes/index');
 
@@ -53,6 +54,8 @@ app.post('/commandes', async (req, res) => {
     let items;
     let reqItem = req.body.items;
     let montant;
+    let schema;
+    let values;
     //let token = jwt.sign({ nom: req.body.nom }, 'my_secret_key');
 
     try{
@@ -70,7 +73,44 @@ app.post('/commandes', async (req, res) => {
 
         montant = await db('item').columns([
             db.raw('sum(tarif * quantite) as total')]).where('command_id', '=', id);
-        
+
+        schema = Joi.object({
+            nom: Joi.string()
+                .min(1)
+                .max(50)
+                .required(),
+
+            mail: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }).required(),
+
+            livraison: Joi.date().greater('now').iso().required(),
+
+            items: Joi.array()
+                .items({
+                    uri: Joi.string().required(),
+                    quantite: Joi.number().required(),
+                    libelle: Joi.string().required(),
+                    tarif: Joi.number().required(),
+                    command_id: Joi.string().required(),
+                })
+
+        });
+
+        values = await schema.validateAsync({
+            nom: req.body.nom,
+            mail: req.body.mail,
+            livraison: req.body.livraison.date + ' ' + req.body.livraison.heure,
+            items: reqItem.map(
+                item => ({
+                    uri: item.uri,
+                    quantite: item.q,
+                    libelle: item.libelle,
+                    tarif: item.tarif,
+                    command_id: id
+                })
+            )
+        });
+
         commande = await db("commande").insert({
             id: id,
             created_at: db.raw('CURRENT_TIMESTAMP'),

aefd0ce9362bba7e2c8a41dc260a8d38b5c0813c
Tania Olivia <taniaolivia9@gmail.com>
2022-02-04 15:46:15 +0100

    TD4 fini et modification de .env et .env.model

--- service/api/app.js
+++ service/api/app.js
@@ -6,6 +6,7 @@ const hateoasLinker = require('express-hateoas-links');
 const db = require('./knex.js');
 
 const indexRouter = require('./routes/index');
+const e = require('express');
 
 const app = express();
 
@@ -23,10 +24,16 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.get('/commandes', async (req, res) => {
     let commande;
-    
+    let result;
+
     try{
         commande = await db.select('id', 'mail', 'created_at', 'montant').from('commande');
-        res.send(commande);
+        result = {
+            type: "collection",
+            count: commande.length,
+            commandes: commande
+        }
+        res.status(200).json(result);
     } 
     catch(error){
         res.status(500).json({
@@ -39,7 +46,10 @@ app.get('/commandes', async (req, res) => {
 
 app.get('/commandes/:id', async (req, res) => {
     let id = req.params.id;
+    let embed = req.query.embed; 
     let commande;
+    let items;
+    let result;
     
     try{
         commande = await db.select('id', 'nom', 'mail', 'created_at', 'livraison', 'montant' ).from('commande').where('id', '=', id);
@@ -52,20 +62,57 @@ app.get('/commandes/:id', async (req, res) => {
                message: "ressource non disponible : /commandes/" + id});
         }
         else{
-            const result = {
-                type: "resource",
-                commande: commande[0]
-            }
-            res.json(result, [
-                {
-                    items: {
-                        href: "http://localhost:3333/commandes/" + commande[0].id + "/items"
-                    },
-                    self : {
-                        href: "http://localhost:3333/commandes/" + commande[0].id
+            if(!embed)
+            {
+                result = {
+                    type: "resource",
+                    commande: {
+                        id: commande[0].id,
+                        mail: commande[0].mail,
+                        nom: commande[0].nom,
+                        date_commande: commande[0].created_at,
+                        date_livraison: commande[0].livraison,
+                        montant: commande[0].montant
                     }
                 }
-            ]);
+                res.status(200).json(result, [
+                    {
+                        items: {
+                            href: "http://localhost:3333/commandes/" + commande[0].id + "/items"
+                        },
+                        self : {
+                            href: "http://localhost:3333/commandes/" + commande[0].id
+                        }
+                    }
+                ]);
+            }
+            else{
+                
+                items = await db.select('id', 'libelle', 'tarif', 'quantite').from('item').where('command_id', '=', commande[0].id);
+
+                result = {
+                    type: "resource",
+                    commande: {
+                        id: commande[0].id,
+                        mail: commande[0].mail,
+                        nom: commande[0].nom,
+                        date_commande: commande[0].created_at,
+                        date_livraison: commande[0].livraison,
+                        montant: commande[0].montant,
+                        items: items
+                    }
+                }
+                res.status(200).json(result, [
+                    {
+                        items: {
+                            href: "http://localhost:3333/commandes/" + commande[0].id + "/items"
+                        },
+                        self : {
+                            href: "http://localhost:3333/commandes/" + commande[0].id
+                        }
+                    }
+                ]);
+            }
         }
     } 
     catch(error){
@@ -81,6 +128,7 @@ app.get('/commandes/:id', async (req, res) => {
 app.put('/commandes/:id', async (req, res) => {
     let id = req.params.id;
     let commande;
+    let update;
     
     try{
         commande = await db.select('id', 'nom', 'mail', 'created_at', 'livraison', 'montant' ).from('commande').where('id', '=', id);
@@ -93,14 +141,12 @@ app.put('/commandes/:id', async (req, res) => {
                message: "ressource non disponible : /commandes/" + id});
         }
         else{
-            db.select('id', 'nom', 'mail', 'created_at', 'livraison', 'montant' ).from('commande').where('id', '=', id).update({
+            update = await db.select('id', 'nom', 'mail', 'created_at', 'livraison', 'montant' ).from('commande').where('id', '=', id).update({
                 nom: req.body.nom,
                 mail: req.body.mail,
                 livraison: req.body.livraison
-            })
-            .then(data => {
-                res.status(204).json('success');
             });
+            res.status(204).json('success');
         }
     } 
     catch(error){
@@ -115,6 +161,8 @@ app.put('/commandes/:id', async (req, res) => {
 app.get('/commandes/:id/items', async (req, res) => {
     let id = req.params.id;    
     let commande;
+    let items;
+    let result;
 
     try {
          commande = await db.select("*").from('commande').where("id", "=", id);
@@ -127,9 +175,13 @@ app.get('/commandes/:id/items', async (req, res) => {
                 message: "ressource non disponible : /commandes/" + id});
          }
          else{
-            db.select('id', 'libelle', 'tarif', 'quantite').from('item').where('command_id', '=', id).then(data => {
-                res.send(data);
-            });
+            items = await db.select('id', 'libelle', 'tarif', 'quantite').from('item').where('command_id', '=', commande[0].id);
+            result = {
+                type: "collection",
+                count: items.length,
+                items: items
+            }
+            res.status(200).json(result);
          }
     } 
     catch(error){
@@ -140,6 +192,7 @@ app.get('/commandes/:id/items', async (req, res) => {
     }
     
 });
+
 
 app.get("*", (req,res)=>{
     res.status(400).json({

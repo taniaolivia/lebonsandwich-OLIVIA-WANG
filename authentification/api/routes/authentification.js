e0991972bdaa9243f5c049cc46639d79f986fb83
Tania Olivia <taniaolivia@MacBook-Pro-de-Tania.local>
2022-03-15 18:06:22 +0100

    modification de docker (ajout api_gateway) et service authentification

--- authentification/api/routes/authentification.js
+++ authentification/api/routes/authentification.js
@@ -4,12 +4,8 @@ const express = require('express');
 const router = express.Router();
 //const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const auth = require('../middleware/verification');
 
-
-router.post('/', async (req, res) => {
-    let nom = req.body.nom_client;
-    let passwd = req.body.passwd;
+router.post('/signin', async (req, res) => {
     let users;
     let user;
 
@@ -53,45 +49,34 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/signin', auth, async (req, res) => {
-    try{
-        res.redirect('http://localhost:3335/auth/signup');
-    }
-    catch(error){
-        res.status(500).json({
-            type: "error",
-            error: "500",
-            message: "erreur lors de la connexion à la base de données"
-        });
-    }
-});
-
-router.get('/signup', auth, async (req, res) => {
-    try{
-        return res.redirect('http://localhost:3334/suivi_commandes');
-    }
-    catch(error){
-        res.status(500).json({
-            type: "error",
-            error: "500",
-            message: "erreur lors de la connexion à la base de données"
-        });
-    }
-});
-
-router.post('/signup', auth, async (req, res) => {
+router.post('/signup', async (req, res) => {
     let user;
 
     try{
-        user = await db("client").insert({
-            nom_client: req.body.nom_client,
-            mail_client: req.body.mail_client,
-            passwd: req.body.passwd
-        });
-     
-        return res.status(201).json({
-            message: "User is created successfully!"
-        });
+        if(req.headers.authorization)
+        {
+            const base64Credentials = req.headers.authorization.split(' ')[1];
+            const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+            [nom, passwd] = credentials.split(':');
+
+            user = await db("client").insert({
+                nom_client: req.headers.nom,
+                mail_client: req.headers.mail,
+                passwd: req.headers.passwd
+            });
+         
+            token = jwt.sign({nom}, 'my_secret_key', {algorithm: 'HS256', expiresIn: '600s'});
+    
+            return res.status(201).json({
+                user: req.headers.nom,
+                token: token
+            });
+        }
+        else{
+            return res.status(401).json({
+                error: "Missing credential"
+            })
+        }
     }
     catch(error){
         res.status(500).json({
@@ -101,24 +86,6 @@ router.post('/signup', auth, async (req, res) => {
         });
     }
 });
-
-
-
-/*router.get("*", (req,res)=>{
-    res.status(400).json({
-        type: "error",
-        error: "400",
-        message: "la requête " + req.url + " est mal formée"});
-});*/
-
-
-/*router.use((req, res)=>{
-    res.json({
-        type: "error",
-        error: "405",
-        message: "erreur de type 405 Method Not Allowed"
-    })
-});*/
 
 module.exports = router;
 
